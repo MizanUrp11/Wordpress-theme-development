b2abfcae8eb480632ddf0a1eb92d822c298d17ee
MizanUrp11 <mrn1105009@gmail.com>
2020-01-14 22:37:24 +0600

    Update before 14th jan

--- wp-content/themes/carnews/index.php
+++ wp-content/themes/carnews/index.php
@@ -1,50 +1,10 @@
 <!DOCTYPE html>
 <html <?php language_attributes();?>>
-
-<head>
-    <!-- Meta Tags -->
-    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+   <?php wp_head() ?>
 
 
-    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+<?php get_header(); ?>
 
-
-    <meta name="description" content="CarCare  -  Car Repairing  and Servicing Html Template Responsive Template">
-    <meta name="keywords" content="Repair ,Servicing ,Mechanic">
-    <meta name="author" content="Theme Moor">
-    <!-- Page Title -->
-
-
-    <title><?php bloginfo( 'name' ) ?></title>
-
-    
-    <!-- Apple Touch Icons &Favicon -->
-    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/images/fav-icon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/images/fav-icon/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/images/fav-icon/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/images/fav-icon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/images/fav-icon/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/images/fav-icon/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/images/fav-icon/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/images/fav-icon/apple-icon-152x152.html">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/images/fav-icon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="<?php echo get_template_directory_uri(); ?>/images/fav-icon/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/images/fav-icon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_template_directory_uri(); ?>/images/fav-icon/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/images/fav-icon/favicon-16x16.png">
-    <!-- Master css File Here -->
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/master.css">
-    <link rel="stylesheet" href="<?php echo get_stylesheet_uri()?>">
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-<?php wp_head() ?>
-
-</head>
 
 <body class="home">
     <!-- start preloader -->
@@ -59,8 +19,68 @@
 
 
     <?php get_header( )?>
+    
+    
+
+<header id="header_area" class="header">
+        <div class="header-top-bar">
+            <div class="container">
+                <div class="row">
+                    <div class="header_top_bar_content">
+                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-9 company">
+                            <div class="company-info">
+                                <ul>
+                                    <li><i class="flaticon-close-envelope"></i><a href="#">example@gmail.com</a></li>
+                                    <li><i class="flaticon-telephone"></i><a href="#">(000)-000-0000</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-3 social">
+                            <div class="header-top-search">
+                                <form action="#">
+                                    <input type="text" placeholder="search">
+                                    <button type="submit"><i class="fa fa-search"></i></button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="nav_area">
+            <div class="header-wrapper">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-3 logo">
+                            <a href="index.html"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt=""></a>
+                            <div class="responsive-menu"></div>
+                        </div>
+                        <div class="col-md-9">
+                            <div class="nav-wrapper">
+                                <nav class="main-navigation">
+                                    
+
+<?php 
+wp_nav_menu(
+    array(
+        'theme_location'    => 'header_menu', 
+        'container_class'   => 'main-navigation', 
+        'items_wrap'        => '<ul class="main-menu">%3$s</ul>',
+        ) );
+?>
 
 
+
+
+
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 
 
     <!-- Header area end -->
@@ -100,7 +120,7 @@
                                         <div class="post-caption transition7s">
                                             <ul>
                                                 <li><i class="fa fa-user"></i><?php the_author();?></li>
-                                                <!-- <li><i class="fa fa-calendar"></i> <?php the_time( 'g:i a' )?> </li> -->
+                                                 <li><i class="fa fa-calendar"></i> <?php the_time( 'g:i a' )?> </li> 
                                                 <li><i class="fa fa-comment"></i> <?php comments_popup_link( 'No Comment', '1 Comment >>', '% Comments');?></li>
                                             </ul>
                                         </div>
@@ -108,7 +128,7 @@
                                     <div class="content">
                                         <h3><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h3>
                                         <div class="text-des">
-                                        <?php the_excerpt();?>
+                                        <?php the_excerpt(); ?>
                                         </div>
 
                                     </div>
@@ -247,47 +267,7 @@
 
     <!-- copyright
     ============================================= -->
-    <div id="copyright-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12 text-center">
-                    <div class="copyright">
-                        <p>&copy; Copyright 2017, All Rights Reserved. <span> A Product Design by <a href="http://thememoor.com"> Theme Moor</a></span></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- /copyright   -->
-    <a href="#" id="scrollTopButton">
-        <i class="fa fa-arrow-up" aria-hidden="true"></i>
-    </a>
-    <!-- Footer Scripts Area -->
-    <!-- All JS File Here -->
-    <script src="<?php echo get_template_directory_uri(); ?>/js/jquery-2.2.4.min.js"></script>
-    <!-- Bootstrap Min Js File -->
-    <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
-    <!--  wow Js File  -->
-    <script src="<?php echo get_template_directory_uri(); ?>/js/wow.min.js"></script>
-    <!-- Owl Carousel -->
-    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/owlcarousel/owl.carousel.min.js"></script>
-    <!-- jquery ui -->
-    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery-ui.min.js"></script>
-    <!-- jquery Counter -->
-    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/waypoints.min.js"></script>
-    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.counterup.min.js"></script>
-    <!-- hero menu -->
-    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/accordian-a2r.js"></script>
-    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.meanmenu.js"></script>
-    <!-- Skillbar -->
-    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/skill.bars.jquery.js"></script>
-    <!-- mixit-up -->
-    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.filterizr.min.js"></script>
-    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/filterize-controls.js"></script>
-    <!-- magnific-popup -->
-    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.magnific-popup.min.js"></script>
-    <!-- Custom JS File -->
-    <script src="<?php echo get_template_directory_uri(); ?>/js/custom.js"></script>
+    
 
 
 
